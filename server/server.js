79511855ffaf4bdf09fbef4cb3e07fafd0848650
Krupal015 <patelkrupald2324@gmail.com>
2026-08-29 02:43:15 +0530

    unauthorized access inngest fixed

--- server/server.js
+++ server/server.js
@@ -1,22 +1,30 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import connectDb from './config/db.js';
+import connectDb from './config/db.js'
 import { clerkMiddleware } from '@clerk/express'
-import {serve } from 'inngest/express'
-import { inngest ,functions } from './inngest/index.js';
-const app = express()
-const port = 3000;
+import { serve } from 'inngest/express'
+import { inngest, functions } from './inngest/index.js'
 
-await connectDb();
+const app = express()
+
+await connectDb()
 
 app.use(express.json())
 app.use(cors())
+
 app.use(clerkMiddleware())
 
+app.get('/', (req, res) => {
+  res.send('server is Live!')
+})
 
-//  app route
-app.get('/', (req, res) => res.send('server is Live!'))
-app.use('/api/inngest',serve({client:inngest,functions}))
+app.use(
+  '/api/inngest',
+  serve({
+    client: inngest,
+    functions,
+  })
+)
 
-app.listen(port, () => console.log(`server listening ar http://localhost:${port}`))
+export default app
